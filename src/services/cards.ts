c3f102390e459f931f9dffbc06ffe033c4bc1a90
Ugnius Ramanauskas <1528409+ugniusramanauskas@users.noreply.github.com>
2023-01-30 22:26:29 +0200

    Added url constant

--- src/services/cards.ts
+++ src/services/cards.ts
@@ -1,14 +1,15 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { BASE_URL } from './constants';
 import type { DeckWithCards } from './types';
 
 export const cardsApi = createApi({
   reducerPath: 'cardsApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://deckofcardsapi.com/api/deck/' }),
+  baseQuery: fetchBaseQuery({ baseUrl: BASE_URL }),
   endpoints: (builder) => ({
-    getDeckWithCards: builder.query<DeckWithCards, void>({
-      query: () => {
+    getDeckWithCards: builder.query<DeckWithCards, number>({
+      query: (count) => {
         return {
-          url: `new/draw/?count=3`,
+          url: `new/draw/?count=${count}`,
         };
       },
     }),
