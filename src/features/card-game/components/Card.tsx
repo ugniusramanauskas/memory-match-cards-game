0b4cfec96d44891d8af65364ca905ddad00a15b9
Ugnius Ramanauskas <1528409+ugniusramanauskas@users.noreply.github.com>
2023-01-30 22:08:19 +0200

    Moved constants

--- src/features/card-game/components/Card.tsx
+++ src/features/card-game/components/Card.tsx
@@ -1,5 +1,5 @@
 import { ICard } from '../../../services/types';
-import { GENERIC_BACK_CARDS_URL } from '../../../services/constants';
+import { GENERIC_CARD_BACK_SIDE_IMAGE } from '../constants';
 import { useAppDispatch, useAppSelector } from '../../../app/hooks';
 import { clickCard } from '../thunks';
 import { selectIsFlipped } from '../selectors';
@@ -10,14 +10,16 @@ type Props = {
 
 export const Card = ({ card }: Props) => {
   const dispatch = useAppDispatch();
-  const { images, id } = card;
+  const { images, id } = card || {};
+  const { png: cardImage } = images || {};
   const isFlipped = useAppSelector((state) => selectIsFlipped(state, id));
   const handleClick = () => dispatch(clickCard(id));
 
+  if (!cardImage) return null;
   return (
     <img
       style={{ maxWidth: '80px', minWidth: '40px' }}
-      src={!isFlipped ? GENERIC_BACK_CARDS_URL : images.png}
+      src={!isFlipped ? GENERIC_CARD_BACK_SIDE_IMAGE : cardImage}
       alt={String(id)}
       onClick={handleClick}
     />
--- src/features/card-game/constants.ts
+++ src/features/card-game/constants.ts
@@ -1,4 +1,4 @@
-export const GENERIC_BACK_CARDS_URL =
+export const GENERIC_CARD_BACK_SIDE_IMAGE =
   'https://opengameart.org/sites/default/files/card%20back%20red.png';
 
 export const CARD_GAME_TOP_SCORES = 'cardGameTopScores';
